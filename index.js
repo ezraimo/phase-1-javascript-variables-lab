4b02d77ca5e99b11800913603426aa35e1b2c358
EZRA KIPRUTO SEREM <ezraserem4038@gmail.com>
2025-03-06 14:42:37 +0300

    added the right info

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+// Code your solution in this file!
+
+const companyName = 'Scuber'; // Must be a constant
+let mostProfitableNeighborhood = 'Chelsea'; // Must be defined with let
+let companyCeo = 'Susan Smith'; // Must be defined with let
+
